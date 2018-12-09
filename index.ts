826610060fa8337a885402536d072b8fd19d95f4
slim <dsslimshaddy@gmail.com>
2018-12-09 16:32:55 +0530

    fix

--- index.ts
+++ index.ts
@@ -15,40 +15,28 @@ import {
   getConfig,
 } from 'app/constants'
 
-class OmniJs {
-  public rel: string
-  public base: string
 
-  constructor(rel: string = '', base: string = '') {
-    this.rel = rel
-    this.base = base
-  }
-  set = (rel: string = '', base: string = '') => {
-    this.rel = rel
-    this.base = base
-  }
-
-
-  generateSeed = (_mnemonic?: string, passphrase: string = '', options?: any) => {
-    const mnemonic = _mnemonic ? _mnemonic : bip39.generateMnemonic(256)
+export const generateSeed = (_mnemonic?: string, passphrase: string = '', options?: any) => {
+  const mnemonic = _mnemonic ? _mnemonic : bip39.generateMnemonic(256)
     //const seed = bip39.mnemonicToSeed(mnemonic, passphrase).slice(0,32)
-    const seed = bip39.mnemonicToSeed(mnemonic, passphrase)
+  const seed = bip39.mnemonicToSeed(mnemonic, passphrase)
     
-    const { wif, address, publicKey } = this.generatePKey(options.config, seed);
+  const { wif, address, publicKey } = generatePKey(options.rel, options.base, options.config, seed);
 
-    return { wif, address, publicKey, mnemonic }
-  }
+  return { wif, address, publicKey, mnemonic }
+}
   /*
   https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#Change
   */
-  generatePKey = (
+export const generatePKey = (
+    rel,
+    base,
     config,
     seed: Buffer,
     account: number = 0,
     change: number = 0,
     index: number = 0
   ) => {
-    const { rel, base } = this;
     const rootNode = getRootNode(seed, rel, base, config)
     const key = deriveAccount(rootNode, account, change, index, rel, config)
     const { wif, address, publicKey } = getWallet(key, rel, base, config)
@@ -56,14 +44,15 @@ class OmniJs {
     return { wif, address, publicKey }
   }
 
-  send = async (
+export const send = async (
+    rel,
+    base,    
     from: string,
     address: string,
     amount: number,
     wif: string,
     options?: any
   ) => {
-    const { rel, base } = this;
       let txid;
       switch (base) {
         case 'ETH':
@@ -82,8 +71,12 @@ class OmniJs {
       }
       return {txid}
   }
-  getTxs = async (address: string, config) => {
-    const { rel, base } = this;
+export const getTxs = async (
+    rel,
+    base,
+    address: string,
+    config
+  ) => {
 
     let data, n_tx, txs = [];
     const api = getConfig(config, rel, base).api;
@@ -91,8 +84,12 @@ class OmniJs {
     txs = await G_IMPORT[base.toLowerCase()].getTxs({ config, rel, base, address });
     return {txs, n_tx};
   }
-  getBalance = async (address: string, config) => {
-    const { rel, base } = this;
+export const getBalance = async (
+    rel,
+    base,    
+    address: string,
+    config
+  ) => {
 
     const api = getConfig(config, rel, base).api;
     let data;
@@ -101,6 +98,4 @@ class OmniJs {
     balances =  await G_IMPORT[base.toLowerCase()].getBalance({ config, rel, base, address });
     return balances;
   }
-}
 
-export default OmniJs
